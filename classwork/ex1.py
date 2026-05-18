242e4aa766fe6ea0c74448dbd97ef36248bdb854
PinkSherlie <aglaiacharis1@gmail.com>
2026-05-18 17:20:54 +0000

    start ex.4

--- classwork/ex1.py
+++ classwork/ex1.py
@@ -2,6 +2,7 @@ class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        number_served = 0
 
     def describe_restaurant(self):
         return f"Restaurant name is: {self.restaurant_name}"
@@ -11,6 +12,7 @@ class Restaurant:
         return f"{self.restaurant_name} is open now!"
 
 restaurant = Restaurant("American diner inn", "American")
+restaurant_1 = 10
 
 
 print(f"Name: {restaurant.restaurant_name}")
@@ -18,3 +20,48 @@ print(f"Cusine: {restaurant.cuisine_type}")
 
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Restaurant:
+#     def __init__(self, restaurant_name, cuisine_type):
+#         self.restaurant_name = restaurant_name
+#         self.cuisine_type = cuisine_type
+
+#     def describe_restaurant(self):
+#         return f"Restaurant name is: {self.restaurant_name}"
+#         return f"Cuisine is: {cuisine_type}"
+
+#     def open_restaurant(self):
+#         return f"{self.restaurant_name} is open now!"
+
+# restaurant = Restaurant("American diner inn", "American")
+
+
+# print(f"Name: {restaurant.restaurant_name}")
+# print(f"Cusine: {restaurant.cuisine_type}")
+
+# restaurant.describe_restaurant()
+# restaurant.open_restaurant()
