89499bb40215d01ba0a6e017afca762d11f85ac3
PinkSherlie <aglaiacharis1@gmail.com>
2026-05-21 10:34:27 +0000

    rewrite classwork ex6.py

--- classwork/ex6.py
+++ classwork/ex6.py
@@ -1,31 +1,24 @@
-class Restaurant:
+class Restaurant():
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
 
     def describe_restaurant(self):
-        return f"Restaurant name is: {self.restaurant_name} Cuisine is: {self.cuisine_type}"
+        return f"Restaurant: {self.restaurant_name}, Cuisine: {self.cuisine_type}"
 
     def open_restaurant(self):
-        return f"{self.restaurant_name} is open now!"
-
+        return "Restaurant is open now!"
+    
 class IceCreamStand(Restaurant):
     def __init__(self, restaurant_name, flavors):
-        super().__init__(restaurant_name, cuisine_type="Ice Cream")
+        super().__init__(restaurant_name, cuisine_type= "Ice cream")
         self.flavors = flavors
 
     def display_flavors(self):
-        print(f"Availble flavors: {self.flavors}")
+        print("Available flavors:")
+        for flavor in self.flavors:
+            print(f"-{flavor}")
 
-my_stand = IceCreamStand("Scooby-Doo ice-cream", ["Vanilla", "Chocolate", "Strawberry"])
+my_stand = IceCreamStand("Scooby-doo", ["vanilla", "chocolate", "strawberry"])
 print(my_stand.describe_restaurant())
 my_stand.display_flavors()
-
-restaurant = Restaurant("American diner inn", "American")
-
-
-print(f"Name: {restaurant.restaurant_name}")
-print(f"Cusine: {restaurant.cuisine_type}")
-
-restaurant.describe_restaurant()
-restaurant.open_restaurant()
