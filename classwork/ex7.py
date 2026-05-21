9c29a34c0b1996175f41afcb6c0326323722f2bd
PinkSherlie <aglaiacharis1@gmail.com>
2026-05-21 10:48:58 +0000

    rewrite ex7.py

--- classwork/ex7.py
+++ classwork/ex7.py
@@ -1,37 +1,31 @@
 class User:
-    def __init__(self, first_name, last_name, mail, phone):
+    def __init__(self, first_name, last_name, phone, mail):
         self.first_name = first_name
         self.last_name = last_name
-        self.mail = mail
         self.phone = phone
+        self.mail = mail
 
     def describe_user(self):
-        return f"User information: {self.first_name}, {self.last_name}, {self.mail}, {self.phone}"
+        return f"User information: {self.first_name}, {self.last_name}, {self.phone}, {self.mail}"
 
     def greet_user(self):
-        return f"Hello {self.first_name}, {self.last_name}"
-
-user1 = User("Anna", "Shhh", "anna@gmail.com", "54666")
-user2 = User("Lika", "Dgggg", "lika@gmail.com", "547866")
-user3 = User("Lana", "YYYhh", "lana@gmail.com", "54266")
-
-print(user1.describe_user())
-print(user2.describe_user())
-print(user3.greet_user())
-
+        return f"Hello to {self.first_name} {self.last_name}"
+    
 class Admin(User):
-    def __init__(self, first_name, last_name, mail, phone, privileges):
-        super().__init__(first_name, last_name, mail, phone)
+    def __init__(self, first_name, last_name, phone, mail, privileges):
+        super().__init__(first_name, last_name, phone, mail)
         self.privileges = privileges
 
     def show_privileges(self):
-        print(f"Administrator privileges for {self.first_name}")
+        print(f"Administrator privilages for {self.first_name}")
         for privilage in self.privileges:
-            print(f" {privilage}")
+            print(f"{privilage}")
 
-admin_rights = ["can add post", "can delete post", "can ban user", "can modify settings"]
-admin_user = Admin("Anna", "Shhh", "anna@gmail.com", "54666", admin_rights)
-admin_user.describe_user()
-admin_user.show_privileges()
+
+admin_privilages = ["can add post", "can delete post", "can ban user"]
+user_1 = Admin("Anna", "KKK", "56788", "kkk@gmail.com", admin_privilages)
+print(user_1.describe_user())
+user_1.show_privileges()
+
 
 
