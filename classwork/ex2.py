586edd36271b074c16bf2e9a25167af007cde864
PinkSherlie <aglaiacharis1@gmail.com>
2026-05-21 07:54:23 +0000

    rewrite ex2.py

--- classwork/ex2.py
+++ classwork/ex2.py
@@ -1,19 +1,23 @@
-class Restaurant:
+class Restaurant():
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
 
     def describe_restaurant(self):
-        return f"Restaurant: {self.restaurant_name} | Cuisine: {self.cuisine_type}"
+        return f"Restaurant: {self.restaurant_name}, Cuisine: {self.cuisine_type}"
 
-restaurant1 = Restaurant("American dinner inn", "American")
-restaurant2 = Restaurant("Sushi paradise", "Asian")
-restaurant3 = Restaurant("Khinkali house", "Georgian")
+    def open_restaurant(self):
+        return "Restaurant is open now!"
 
-restaurant1.describe_restaurant()
-restaurant2.describe_restaurant()
-restaurant3.describe_restaurant()
 
-print(f"Name: {restaurant1.restaurant_name}. Cusine: {restaurant1.cuisine_type}")
-print(f"Name: {restaurant2.restaurant_name}. Cusine: {restaurant2.cuisine_type}")
-print(f"Name: {restaurant3.restaurant_name}. Cusine: {restaurant3.cuisine_type}")
+restaurant_1 = Restaurant("American dinner inn", "American")
+restaurant_2 = Restaurant("Sushi Paradise", "Asian")
+restaurant_3 = Restaurant("Khinkali House", "Georgian")
+
+restaurant_1.describe_restaurant()
+restaurant_2.describe_restaurant()
+restaurant_3.describe_restaurant()
+
+print(f"Restaurant: {restaurant_1.restaurant_name}, Cuisine: {restaurant_1.cuisine_type}")
+print(f"Restaurant: {restaurant_2.restaurant_name}, Cuisine: {restaurant_2.cuisine_type}")
+print(f"Restaurant: {restaurant_3.restaurant_name}, Cuisine: {restaurant_3.cuisine_type}")
