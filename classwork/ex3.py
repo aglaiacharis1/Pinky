a4da0fccae33397c788099b7ac6965b044d0beda
PinkSherlie <aglaiacharis1@gmail.com>
2026-05-21 09:47:28 +0000

    rewrite ex3.py

--- classwork/ex3.py
+++ classwork/ex3.py
@@ -1,21 +1,25 @@
 class User:
-    def __init__(self, first_name, last_name, mail, phone):
+    def __init__(self, first_name, last_name, phone, mail):
         self.first_name = first_name
         self.last_name = last_name
-        self.mail = mail
         self.phone = phone
+        self.mail = mail
 
     def describe_user(self):
-        return f"User information: {self.first_name}, {self.last_name}, {self.mail}, {self.phone}"
+        return f"User information: {self.first_name}, {self.last_name}, {self.phone}, {self.mail}"
 
     def greet_user(self):
-        return f"Hello {self.first_name}, {self.last_name}"
+        return f"Hello to {self.first_name} {self.last_name}"
 
-user1 = User("Anna", "Shhh", "anna@gmail.com", "54666")
-user2 = User("Lika", "Dgggg", "lika@gmail.com", "547866")
-user3 = User("Lana", "YYYhh", "lana@gmail.com", "54266")
+user_1 = User("Anna", "KKK", "56788", "kkk@gmail.com")
+user_2 = User("Nina", "JJJJ", "8477", "lll@gmail.com")
+user_3 = User("Lana", "MMM", "84762", "mmm@gmail.com")
 
-print(user1.describe_user())
-print(user2.describe_user())
-print(user3.greet_user())
+print(user_1.describe_user())
+print(user_1.greet_user())
 
+print(user_2.describe_user())
+print(user_2.greet_user())
+
+print(user_3.describe_user())
+print(user_3.greet_user())
